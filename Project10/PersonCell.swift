c8fea95441bfd08c41f9a2c92e7bed87884df394
Max Zykin <sanchez.max@yandex.ru>
2023-08-09 16:41:16 +0400

    done

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Aleksey Novikov on 08.08.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
